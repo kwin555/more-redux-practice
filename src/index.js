7fa7c5153ed760afd937d6179d39f84c4e5ccf46
kwin555 <ktn555@att.net>
2020-01-21 20:41:01 -0600

    jsx basics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-//Import React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-//create a react component
+
 const App = () => {
-    return <div>Hi there !</div>
-}
-//take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+    const getButtonText = () => {
+        return 'Click on me!';
+    };
+    const style = { backgroundColor: 'blue', color: 'white' };
+    const labelTest = 'Enter name:';
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name;{' '}
+            </label>{' '}
+            <input id="name" type="text" />
+            <button style={style}> {getButtonText()} </button>{' '}
+        </div>
+    );
+};
+ReactDOM.render(<App />, document.querySelector('#root'));
